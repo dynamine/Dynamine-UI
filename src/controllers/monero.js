bd465bfc141464a5213ed88da97311d50a010e23
Gfuen <fuentesg@spu.edu>
2018-05-11 09:00:46 -0700

    Get Monero running with static data

--- src/controllers/monero.js
+++ src/controllers/monero.js
@@ -16,7 +16,51 @@
         viewFactory.title = 'Monero';
         viewFactory.prevUrl = null;
         let coinName = "monero";
-        
+        var walletAddress;
+        var walbal;
+        var walnumtrans;
+        var payments;
+        var coinchart;
+
+        walletAddress = callmoneroWallet.callconfig(dynamineConfig);
+        walbal = callmoneroWallet.callwalletbal(walletAddress);
+        walnumtrans = callmoneroWallet.callwalletnumtrans(walletAddress);
+        payments = callmoneroWallet.callwallettrans(walletAddress);
+        console.log("Transactions: ");
+        console.log(payments);
+
+        $scope.getWalletBalance = function() {
+            walbal = '' + walbal;
+            return walbal;
+        }
+
+         $scope.getWalletNumTrans = function() {
+             walnumtrans = '' + walnumtrans;
+             return walnumtrans;
+         }
+
+
+        $scope.getWalletPayment1 = function() {
+            console.log("Transaction type: " + typeof(payments));
+            console.log(payments);
+            return payments[0];
+        }
+
+        $scope.getWalletPayment2 = function() {
+            return payments[1];
+        }
+
+        $scope.getWalletPayment3 = function() {
+            return payments[2];
+        }
+
+        $scope.getWalletPayment4 = function() {
+            return payments[3];
+        }
+
+        $scope.getWalletPayment5 = function() {
+            return payments[4];
+        }
 
 
         $scope.resources = dynamineConfig.getResources();
@@ -53,16 +97,22 @@
         }
 
         $scope.refreshWalletTokens = function(master) {
-          createChart('#MoneroWalletChart', {
+          coinchart = createChart('#MoneroWalletChart', {
               type: 'line',
               data: { labels: [], datasets: [{
-                  data: [ 0.01, 0.025, 0.011, 0.02],
+                  data: [ 64584, 78655, 98979, 7775, 7655, 9887, 7422, 8483, 9580, 9676, 8755, 7755, 8657, 6589, 8893, 9657, 8896, 7742, 7466, 7332, 7433, 8543, 9053, 8743],
                   label: 'coins',
                   backgroundColor: ['rgba(24, 138, 226, 0.5)', 'rgba(16, 196, 105, 0.5)', 'rgba(128, 197, 218, 0.5)',
                       'rgba(248, 142, 15, 0.5)', 'rgba(207, 32, 241, 0.5)', 'rgba(91, 105, 188, 0.5)', 'rgba(24, 138, 226, 0.5)']
                   //backgroundColor:['#10C469', '#FFCE56']
               }]}
           });
+
+          setInterval(function(){
+            var payments = callzcashWallet.callwallettrans(walletAddress);
+            coinchart.update(payments);
+            }, 100000);
+
 
           if(!master || master !== true)
               toast.success('Timers data has been updated');
